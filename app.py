cd835df6ae47538105b2d96dfb856397b79732e2
Jared <jaredliw@gmail.com>
2025-10-01 11:01:41 +0800

    Outline checkout page

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import streamlit as st
+from pathlib import Path
 from utils import Database
 
 db = Database()
-
+assets_dir = Path("./assets")
+if "cart" not in st.session_state or not isinstance(st.session_state.cart, dict):
+    st.session_state.cart = {}
 st.set_page_config(page_title="SUTD Kopitiam", page_icon="☕", layout="wide")
+st.markdown("""
+    <style>
+    .stSidebar {
+        display: none;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 st.title("SUTD Kopitiam", anchor=False)
-st.image("./assets/cover.jpg")
+st.image(assets_dir / "cover.jpg")
 
 categories = db.read("categories")
 tabs = st.tabs(categories)
@@ -25,11 +36,19 @@ for i, category in enumerate(categories):
                 item = items[j]
 
                 with col:
-                    st.image("./assets/" + item["name"] + ".jpg")
+                    st.image(assets_dir / f"{item['name']}.jpg")
                     st.subheader(item["name"], divider="gray", anchor=False)
                     st.write(f"Price: ${item['price']:.2f}")
                     st.write(item["desc"])
                     with st.columns([1, 1])[0]:
-                        st.number_input("No. of Items", min_value=0, step=1, key=item["name"],
+                        num = st.number_input("No. of Items", min_value=0, step=1, key=item["id"],
                                         label_visibility="collapsed")
+                        st.session_state.cart[item["id"]] = num
+                        if num <= 0:
+                            del st.session_state.cart[item["id"]]
                 j += 1
+
+st.divider()
+st.write(f"No. of Items in Cart: {sum(st.session_state.cart.values())}")
+if st.button("Go to Checkout"):
+    st.switch_page("./pages/checkout.py")
---
+++ pages/checkout.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+if "cart" not in st.session_state or not isinstance(st.session_state.cart, dict):
+    st.session_state.cart = {}
+st.set_page_config(page_title="SUTD Kopitiam", page_icon="☕", layout="wide")
+st.markdown("""
+    <style>
+    .stSidebar {
+        display: none;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.title("SUTD Kopitiam - Checkout", anchor=False)
+st.write(st.session_state.cart)
+
+if st.button("Back to Memu", key="back_to_menu"):
+    st.switch_page("./app.py")
+if st.button("Checkout", key="checkout"):
+    st.toast("Checkout successful!")
+    st.session_state.cart = {}
